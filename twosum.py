904a5497bdba9a43b49a75fad18dfddbc0f7e85a
Patrick Godin <PatrickJohn.Godin@gmail.com>
2022-01-01 21:46:01 -0400

    Trivial solution

--- twosum.py
+++ twosum.py
@@ -1,3 +1,7 @@
 class Solution:
+    #Double for loops, O(n^2) time, O(1) Space
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[j] == target - nums[i]:
+                    return [i, j]
